0b00090fa19f4a7c4c57b1f58e52dde17abbc59f
qq33158 <80340308+qq33158@users.noreply.github.com>
2024-01-13 00:12:42 +0800

    edit/delete member function

--- ProjectMVC/Controllers/MemberController.cs
+++ ProjectMVC/Controllers/MemberController.cs
@@ -8,7 +8,7 @@ namespace ProjectMVC.Controllers
     public class MemberController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public MemberController( ApplicationDbContext db)
+        public MemberController(ApplicationDbContext db)
         {
             _db = db;
         }
@@ -18,19 +18,66 @@ namespace ProjectMVC.Controllers
             List<Member> objMemberList = _db.Members.ToList();
             return View(objMemberList);
         }
-        public IActionResult Create() { 
-        
+        public IActionResult Create()
+        {
+
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Member member) {
+        public IActionResult Create(Member member)
+        {
             if (ModelState.IsValid)
-            {   
+            {
                 _db.Members.Add(member);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
-            }        
+            }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            Member? memberFormDb = _db.Members.Find(id);
+            if (memberFormDb == null)
+            {
+                return NotFound();
+            }
+            return View(memberFormDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Members.Update(member);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Member? memberFormDb = _db.Members.Find(id);
+            if (memberFormDb == null)
+            {
+                return NotFound();
+            }
+            return View(memberFormDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Member? memberFormDb = _db.Members.Find(id);
+            if (memberFormDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Members.Remove(memberFormDb);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
