1bf44b7c8999fe9340951f8e51279541813462d0
qq33158 <80340308+qq33158@users.noreply.github.com>
2024-01-12 22:00:23 +0800

    create member contorller and view

---
+++ ProjectMVC/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
